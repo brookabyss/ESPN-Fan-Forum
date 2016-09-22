865c01a48963e621a33271ec01a1a82d828a7f75
Brook Kebede <brookabyss@gmail.com>
2016-09-22 22:03:13 +0000

    Add styling and authentication to controllers

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,6 @@
 class MessagesController < ApplicationController
+    before_action :authenticate_user!, except: [:index, :show]
     before_action :find_message, only: [:show, :edit, :update, :destroy]
-    before_filter :authenticate_user!  
     
     def index
         @messages= Message.all.order("created_at DESC")
